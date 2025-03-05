5a31e0ecdfb198af4006546d437005215c5b1037
Liza <lizka4231@gmail.com>
2025-03-05 21:27:29 +0300

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import React, { FC } from 'react';
 import MainPage from './pages/MainPage';
 import ErrorPage from './pages/ErrorPage';
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App: FC = () => {
   return (
-    <>
-      <MainPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DataProvider } from '../context/DataContext';
 import Circles from '../components/layout/Circles';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
@@ -8,6 +9,7 @@ const ErrorPage = () => {
         <Circles />
         <main className='main--error'>
             <h2 className="main--error__title">Ошибка, запрашиваемая страница не найдена</h2>
+            <Link to='/' className='main--error__link'>Вернуться на главную страницу</Link>
         </main>
     </DataProvider>
   )
