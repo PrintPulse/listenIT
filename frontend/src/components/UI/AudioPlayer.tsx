068885a3874b2ea4f96a304675f4594d0f1a0d54
lizka <lizka4231@gmail.com>
2025-03-26 23:17:13 +0300

    added volume control

--- frontend/src/components/UI/AudioPlayer.tsx
+++ frontend/src/components/UI/AudioPlayer.tsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect, useRef } from "react";
 
 const AudioPlayer: React.FC<{ streamUrl: string }> = ({ streamUrl }) => {
    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+   const [volume, setVolume] = useState<number>(0.5);
    const audioRef = useRef<HTMLAudioElement>(null);
 
    useEffect(() => {
       if (audioRef.current) {
          audioRef.current.src = streamUrl;
+         audioRef.current.volume = volume;
          audioRef.current
             .play()
             .then(() => {
@@ -19,11 +21,33 @@ const AudioPlayer: React.FC<{ streamUrl: string }> = ({ streamUrl }) => {
       }
    }, [streamUrl]);
 
+   useEffect(() => {
+      if (audioRef.current) {
+        audioRef.current.volume = volume;
+      }
+   }, [volume]);
+
+   useEffect(() => {
+      if (audioRef.current) {
+         if (isPlaying) {
+            audioRef.current.play().catch(error => console.error(error));
+         } 
+         else {
+            audioRef.current.pause();
+         }
+      }
+   }, [isPlaying]);
+
    return (
       <div className="audio-container">
          <audio ref={audioRef} className="queue__curr-playing__audio"/>
          <div className="audio-controls">
             <button onClick={ () => setIsPlaying(prevVal => !prevVal) } className={`audio-controls__button audio-controls__button--${isPlaying ? 'pause' : 'play'}`} aria-label={`${isPlaying ? 'pause' : 'play'}`}></button>
+            <div className="audio-controls__volume-control">
+               <input className={'audio-controls__volume-input' + (volume === 0 ? ' muted' : '')} type="range" min="0" max="1" step="0.01" aria-label="Регулировка громкости"
+                  onChange={(e) => setVolume(e.target.valueAsNumber)} value={volume}   
+               />
+            </div>
          </div>
       </div>
 
